bb1cd022228aabf96849c43c9eaba45082e43c5a
Igor Serganov <caesarandroidru@gmail.com>
2019-07-07 22:49:41 +0300

    footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material';
+import { MatIconModule, MatDividerModule, MatButtonModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -13,11 +13,13 @@ import { ToastrModule } from 'ngx-toastr';
     BrowserModule,
     AppRoutingModule,
     MatIconModule,
+    MatButtonModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot({
       closeButton: true,
 
-    })
+    }),
+    MatDividerModule
   ],
   providers: [
     { provide: API_BASE_URL, useValue: 'http://localhost:2006' }
